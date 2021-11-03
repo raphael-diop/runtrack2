22139c170aaca91fde96ae5f89bdae86af9976b5
Raphaël Diop <raphael.diop@laplateforme.io>
2021-11-03 16:48:37 +0100

    job 3

---
+++ jour02/job3/index.php
@@ -0,0 +1,19 @@
+<?php
+for($i = 0; $i <= 100; $i++){
+    if($i >= 0 && $i<=20){
+    echo "<i>".$i."</i>"."<br/>";        
+    }
+    
+    else if($i == 42){
+        echo "LaPlateforme_" . "<br/>";
+    }
+    else if($i >= 25 && $i<= 50){
+        echo "<u>".$i."</u>" . "<br/>";
+    }
+    else {
+        echo $i . "<br/>";
+    }
+    
+} 
+?>
+   

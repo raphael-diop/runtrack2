8c8176c0d4973b77949512ad2a1242aa77a5d64b
Raphaël Diop <raphael.diop@laplateforme.io>
2021-11-03 15:08:40 +0100

    numeros effacés

---
+++ jour02/job2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+for( $var = 0 ; $var < 3233; $var ++){
+    if($var == 26 || $var == 37 || $var == 88 || $var == 1111 || $var == 3233){
+        echo "<br/>";
+    }
+    else if($var <= 1337){
+        echo $var . "<br/>";
+    }
+    
+}
+
+?>
